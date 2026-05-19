320a6f2b01e343e5ccaf018b9e0a73815af90e64
Rocco <roccominetola24@gmail.com>
2026-05-19 12:34:24 -0400

    Add project files.

---
+++ TechJockeys/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace TechJockeys.Models
+{
+    public class Category
+    {
+        // using (ModelName)Id as the property tells the server automatically that this is the primary key for the table
+        public int CategoryId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
